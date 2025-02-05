c0cceee558dedb31d492303ffeffd45015141576
KennOtieno <otiken001@gmail.com>
2025-02-05 02:37:55 -0800

    New Changes

--- dart/basics/variables.dart
+++ dart/basics/variables.dart
@@ -1,9 +1,22 @@
 void main() {
+  // Declared the variables as told in the comment below.
   var a = 43.434;
   const b = "Hey, this is a string.";
   final c = 343;
+  // Printed their Values as told.
+  print('a');
+  print('b');
+  print('c');
 
-  a = 3446435;
+  a = 34646; // Created a double value then converted it into an int
 
-  print('$b , $a changed variable , and $c final variable.');
+  print(' $b While $c is an int.'); // Declared a String and an Int and concatenated them to form a sentence
 }
+
+/*
+    Dart Variables & Data Types Exercise
+
+1. Declare variables using var, const, and final, and print their values.
+2. Create a double variable, assign it a value, then convert it to an int.
+3. Declare a String and an int, then concatenate them to form a sentence.
+ */
