a77f5aa6b4db1e311375d0412e9ea70b7946e2b5
KennOtieno <otiken001@gmail.com>
2025-02-05 22:58:27 -0800

    Dart Operators

---
+++ dart/basics/operators/arithmetic_operators.dart
@@ -0,0 +1,18 @@
+void main() {
+  var num1 = 25;
+  var num2 = 7;
+
+  print(num1 + num2);
+  print(num1 - num2);
+  print(num1 * num2);
+  print(num1 / num2);
+  print(num1 ~/ num2);
+  print(num1 % num2);
+}
+/*
+                 QUESTION.
+Write a program that:
+
+Declares two numbers (num1 = 25, num2 = 7).
+Prints their sum, difference, product, division, integer division, and remainder
+ */
