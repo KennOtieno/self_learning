d2b52a3b96599d8a6dc9a1ada1694aaa338de82f
KennOtieno <otiken001@gmail.com>
2025-02-06 00:11:24 -0800

    Logical Operators.

---
+++ dart/basics/operators/logical_operators.dart
@@ -0,0 +1,19 @@
+void main() {
+  var isSunny = true;
+  var isWarm = false;
+
+  print(isSunny && isWarm);
+
+  print(isSunny || isWarm);
+
+  print(!isSunny);
+}
+/*
+      Logical Operators
+Declare two boolean variables: isSunny = true, isWarm = false.
+Use &&, ||, and ! to print the results of different logical expressions.
+                EXPECTED OUTPUT
+                false
+                true
+                false
+ */
