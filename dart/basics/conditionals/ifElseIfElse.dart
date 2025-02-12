a70d5e53b5a9e6ee1c0fb1586bd124e68a034e1f
KennOtieno <otiken001@gmail.com>
2025-02-12 06:43:32 -0800

    Corrected

--- dart/basics/conditionals/ifElseIfElse.dart
+++ dart/basics/conditionals/ifElseIfElse.dart
@@ -1,6 +1,6 @@
 void classifyNumber(int number) {
   if(number > 0) {
-    print("Negative");
+    print("Positive");
   } else if(number < 0) {
     print("Negative");
   } else {
@@ -13,3 +13,12 @@ void main() {
   classifyNumber(0);
   classifyNumber(765);
 }
+/*
+   Write a function called classifyNumber that:
+
+Takes an integer as input.
+Uses an if-else if-else structure to:
+Print "Positive" if the number is greater than 0.
+Print "Negative" if the number is less than 0.
+Print "Zero" if the number is 0.
+ */
