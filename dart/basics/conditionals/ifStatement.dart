17887c4bb21056fd4b7c0f8dae274b515e50ce09
KennOtieno <otiken001@gmail.com>
2025-02-12 06:09:56 -0800

    conditionals

---
+++ dart/basics/conditionals/ifStatement.dart
@@ -0,0 +1,18 @@
+void checkPositive(int number) {
+  if(number > 0) {
+    print("The number is positive");
+  }
+}
+
+void main() {
+  checkPositive(54);
+  checkPositive(-1);
+  checkPositive(0);
+}
+/*
+      Exercise 1: Basic if Statement
+  Write a function called checkPositive that:
+    Takes an integer as input.
+Uses an if statement to check if the number is greater than 0.
+If it is, prints "The number is positive"
+ */
