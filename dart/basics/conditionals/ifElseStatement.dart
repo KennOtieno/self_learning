16d0602c741a276be8452d060567b6a4a1bd5b90
KennOtieno <otiken001@gmail.com>
2025-02-12 06:19:37 -0800

    if else Statement

---
+++ dart/basics/conditionals/ifElseStatement.dart
@@ -0,0 +1,19 @@
+void checkEvenOdd(int number) {
+  
+  if(number == 2) {
+    print("Even");
+  } else {
+    print("Odd");
+  }
+}
+
+void main() {
+  checkEvenOdd(4);
+}
+/*
+    Write a function called checkEvenOdd that:
+
+Takes an integer as input.
+Uses an if-else statement to check whether the number is even or odd.
+Prints "Even" if the number is even, and "Odd" if the number is odd.
+ */
